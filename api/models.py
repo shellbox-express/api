548af77747ed2c2d6d0ac32eb316f7f2da7bfbf2
Felipe Rodrigues <felipe@felipevr.com>
2019-03-24 03:18:01 -0300

    Define basic models

---
+++ api/models.py
@@ -0,0 +1,21 @@
+from peewee import *
+from os import environ
+
+host = environ.get("SERVER")
+
+db = MySQLDatabase(
+    host=host, port=3303, passwd="shellshell", user="shell", database="shell"
+)
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Purchase(BaseModel): 
+    date = DateField() 
+    price = FloatField() 
+    qtd = IntegerField() 
+    station = CharField() 
+    
+    class Meta: 
+        table_name = "purchases"
