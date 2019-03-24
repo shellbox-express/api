25b2b156e6ce529b1add91b258b6e6358f6de570
ca-pettarelli <caroline.pettarelli@usp.br>
2019-03-23 21:24:17 -0300

    implementa autenticacao

--- api/auth.py
+++ api/auth.py
@@ -1,2 +1,15 @@
+from requests.auth import HTTPBasicAuth
+from requests import post 
+
 class Auth:
-    pass
+    def __init__(self, client_id, client_secret, base_url="https://api-hackaraizen.sensedia.com/oauth/"):
+    	self.auth = HTTPBasicAuth(client_id, client_secret)
+    	self.base_url = base_url
+    def token(self):
+    	body = {
+    		"grant_type": "client_credentials"
+    	}
+    	r = post(self.base_url + "access-token", json=body, auth=self.auth)
+    	r.raise_for_status()
+    	return r.json()
+    	
